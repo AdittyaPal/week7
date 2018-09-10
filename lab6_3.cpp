f91d0e9e1af1f02a8e0fa1c520518a644b51f56b
AdittyaPal <42172854+AdittyaPal@users.noreply.github.com>
2018-09-10 03:50:15 -0400

    Update lab6_3.cpp

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,7 +1,9 @@
+//include the library
 #include <iostream>
 using namespace std;
 int findMax(int,int);
 void findMaximum(int&,int&,int&);
+//decalre the main function
 int main()
 {
 	int n1,n2=0;
@@ -22,6 +24,7 @@ int main()
 	cout<<"The maximun is "<<max<<endl;
 	return 0;
 }
+//function to calculate the maximum using call by value
 int findMax(int a, int b)
 {
 	if(a>b)
@@ -29,6 +32,7 @@ int findMax(int a, int b)
 	else
 	return b;
 }
+//function to calculate the maximum using call by reference
 void findMaximum(int& a, int& b, int& max)
 {
 	if(a>b)
